1936cfe51057574bb41a309626eca1763df676f4
sinspired <ggmomo@gmail.com>
2026-05-26 12:59:52 +0800

    fix: 更新 setSysProcAttr 函数以包含创建新进程组和隐藏窗口的标志

--- assets/sub_run_windows.go
+++ assets/sub_run_windows.go
@@ -9,6 +9,9 @@ import (
 
 func setSysProcAttr(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+		// CREATE_NEW_PROCESS_GROUP: 独立进程组，不响应 Ctrl+C
+		// CREATE_NO_WINDOW (0x08000000): 完全不创建控制台窗口
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP | 0x08000000,
+		HideWindow:    true,
 	}
 }
